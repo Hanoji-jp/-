2af48497ad5ec7df8df629528556e16b62fc1748
HanojiKD <kd1408698@st.kobedenshi.ac.jp>
2026-07-22 11:54:30 +0900

    dadsadadadada

---
+++ 3DBaseframework/Src/Application/GameObject/UI/GameUI.cpp
@@ -0,0 +1,17 @@
+﻿#include "GameUI.h"
+#include "../Water/Water.h"
+
+void GameUI::Init()
+{
+	// TODO: 判定結果の初期化
+}
+
+void GameUI::Update()
+{
+	// TODO: 水位と目標ラインを比較して判定結果を更新
+}
+
+void GameUI::DrawSprite()
+{
+	// TODO: 判定結果に応じて「良！」などの演出を2D描画
+}
