67098b7fc9ef5eff35cb1ab7a3fab3425161b0cc
kd1439628 <kd1439628@st.kobedenshi.ac.jp>
2026-07-22 12:02:11 +0900

    Titleシーン 点滅
    Titltシーンのプレスボタンの点滅を追加

--- 3DBaseframework/Src/Application/Scene/TitleScene/TitleScene.h
+++ 3DBaseframework/Src/Application/Scene/TitleScene/TitleScene.h
@@ -4,13 +4,21 @@
 
 class TitleScene : public BaseScene
 {
-public :
+public:
 
-	TitleScene()  { Init(); }
+	TitleScene() { Init(); }
 	~TitleScene() {}
 
-private :
+	void Draw()override;
+	void Update() override;
+
+private:
 
 	void Event() override;
 	void Init()  override;
+
+	float m_alphaAdd = 0.01f;
+	float m_alpha = 1.0f;
+
+
 };
