01d5dccb073cac570b55fb109b1cb60d959b9510
hana <kd1394207@st.kobedenshi.ac.jp>
2026-07-23 10:22:13 +0900

    UIと明滅,コメントの追加

--- 3DBaseframework/Src/Application/GameObject/GameOver/GameOver.cpp
+++ 3DBaseframework/Src/Application/GameObject/GameOver/GameOver.cpp
@@ -2,16 +2,46 @@
 
 void GameOver::Init()
 {
-	// 文字
+	// 文字（終了）
 	m_polygon = std::make_shared<KdSquarePolygon>();
-	m_polygon->SetMaterial("Asset/Textures/sipai.png");
+	m_polygon->SetMaterial("Asset/Textures/gameover.png");
+
+	// 大きさと位置（終了）
+	m_polygon->SetScale(1.2f);
+	m_textOffset = { 0.0f, 0.4f, 0.0f };
+
+	// たいとる,やりなおし
+	m_result = std::make_shared<KdSquarePolygon>();
+	m_result->SetMaterial("Asset/Textures/resultUI.png");
+
+	// 大きさと位置（たいとる,やりなおし）
+	m_result->SetScale(1.0f);
+	m_resultOffset = { -0.1f,-0.3f,0.0f };
 
 	// 黒背景
 	m_darkOverlay = std::make_shared<KdSquarePolygon>();
 	m_darkOverlay->SetMaterial("Asset/Textures/kuro.png");
+
+	// 大きさと透明度（黒背景）
 	m_darkOverlay->SetScale(100.0f);
-	std::vector<Math::Color> darkColors(4, Math::Color(0.0f, 0.0f, 0.0f, 0.8f));
+	std::vector<Math::Color> darkColors(4, Math::Color(0.0f, 0.0f, 0.0f, 0.6f));
 	m_darkOverlay->SetVertexColor(darkColors);
+}
+
+void GameOver::Update()
+{
+	// リザルトの明滅
+	if (!m_result) return;
+
+	m_frame++;
+
+	float t = (std::sinf(m_frame * 0.05f) + 1.0f) * 0.5f;
+
+	// 明滅範囲（完全に消したいなら 0.0f に変更)
+	float alpha = 0.2f + 0.7f * t;
+
+	std::vector<Math::Color> resultColors(4, Math::Color(0.0f, 0.0f, 0.0f, alpha));
+	m_result->SetVertexColor(resultColors);
 }
 
 void GameOver::DrawLit()
@@ -26,6 +56,9 @@ void GameOver::DrawLit()
 
 		KdShaderManager::Instance().UndoBlendState();
 	}
-
-	KdShaderManager::Instance().m_StandardShader.DrawPolygon(*m_polygon, m_mWorld);
+	Math::Matrix textWorld = m_mWorld * Math::Matrix::CreateTranslation(m_textOffset);
+	KdShaderManager::Instance().m_StandardShader.DrawPolygon(*m_polygon, textWorld);
+	
+	Math::Matrix resultWorld = m_mWorld * Math::Matrix::CreateTranslation(m_resultOffset);
+	KdShaderManager::Instance().m_StandardShader.DrawPolygon(*m_result, resultWorld);
 }
