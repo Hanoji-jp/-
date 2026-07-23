0389ab100890891b26d847d92e3fb31b650f8d4f
kd1399062 <kd1399062@st.kobedenshi.ac.jp>
2026-07-23 14:39:52 +0900

    ゲームシーンのコードの先頭をそろえただけ

--- 3DBaseframework/Src/Application/Scene/GameScene/GameScene.cpp
+++ 3DBaseframework/Src/Application/Scene/GameScene/GameScene.cpp
@@ -86,10 +86,10 @@ void GameScene::Init()
 	m_objList.push_back(gameover);
 	m_wpGameOver = gameover;
 	//====================
-		// オブジェクト
-		//====================
-		//----- エフェクト -----
-		// 良
+	// オブジェクト
+	//====================
+	//----- エフェクト -----
+	// 良
 	std::shared_ptr<RyouEffect> _spRyou;
 	_spRyou = std::make_shared<RyouEffect>();
 	m_objList.push_back(_spRyou);
