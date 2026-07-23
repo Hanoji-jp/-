96f40a27528c1901d38747d91e3bfb11ea00631b
kd1439628 <kd1439628@st.kobedenshi.ac.jp>
2026-07-23 14:40:43 +0900

    窓（インテリア）追加

---
+++ 3DBaseframework/Src/Application/GameObject/Window/Window.cpp
@@ -0,0 +1,23 @@
+﻿#include "Window.h"
+
+void Window::Init()
+{
+	if (!m_spModel)
+	{
+		m_spModel = std::make_shared<KdModelData>();
+
+		m_spModel->Load("Asset/Data/Models/window/window.gltf");
+
+		Math::Matrix transMat = Math::Matrix::CreateTranslation(50, -5, 70);
+		Math::Matrix scaleMat = Math::Matrix::CreateScale(30.0f);
+		Math::Matrix rotYMat = Math::Matrix::CreateRotationY(DirectX::XMConvertToRadians(0));
+		m_mWorld = scaleMat * rotYMat * transMat;
+	}
+}
+
+void Window::DrawLit()
+{
+	if (!m_spModel) return;
+
+	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel, m_mWorld);
+}
